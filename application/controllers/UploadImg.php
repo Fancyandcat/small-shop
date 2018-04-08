bf672f43fc881c9c93fbafeddc85a72e906f11dd
fancy <1820827184@qq.com>
2018-04-08 18:25:20 +0800

    three

--- application/controllers/UploadImg.php
+++ application/controllers/UploadImg.php
@@ -1,10 +1,10 @@
 <?php
-require "vendor/qiniu/php-sdk/autoload.php";
+require "vendor/autoload.php";
 
 use Qiniu\Auth;
 
 class Upload {
-	function avatar() {
+	function getToken() {
         $accessKey = 'sO48RbRFx4lXWZ7zYj7A7VwMWehqsp1IceEtllZe';
         $secretKey = 'BixrNKnLqlwVeOPG3Iuj1DLvs1-awqUwU1movSgs';
         $auth = new Auth($accessKey, $secretKey);
